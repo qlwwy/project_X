3219e33acc4b51232403bd0d937f0ce3038f124c
qlwwy <quewwfy@gmail.com>
2025-03-28 10:23:34 +0300

    дз 12.2

--- src/masks.py
+++ src/masks.py
@@ -1,3 +1,21 @@
+import logging
+import os
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.FileHandler('logs/masks.log', mode='w')
+file_handler.setLevel(logging.DEBUG)
+
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+logger.addHandler(file_handler)
+
+
 def get_mask_card_number(card_info):
     """
     Принимает на вход строку с типом и номером карты и возвращает маску номера по правилу XXXX XX** **** XXXX
@@ -6,8 +24,9 @@ def get_mask_card_number(card_info):
     card_number = parts[-1]
     if len(card_number) != 16:
         raise ValueError("Номер карты должен содержать 16 цифр.")
-    masked_number = f"{card_number[:4]} {card_number[4:6]} **** {card_number[-4:]}"
-    return " ".join(parts[:-1]) + " " + masked_number
+    masked_card = f"{card_number[:4]} {card_number[4:6]} **** {card_number[-4:]}"
+    logger.info(f"Маскированный номер карты: {masked_card}")
+    return " ".join(parts[:-1]) + " " + masked_card
 
 
 def get_mask_account(account_info):
@@ -19,4 +38,8 @@ def get_mask_account(account_info):
     if len(account_number) < 6:
         raise ValueError("Номер счета должен содержать не менее 6 цифр.")
     masked_number = f"{account_number[-4:]}"
+    logger.info(f"Маскированный номер карты: {masked_number}")
     return " ".join(parts[:-1]) + " " + masked_number
+
+print(get_mask_account('36172638712'))
+print(get_mask_card_number('2737193628281631'))
